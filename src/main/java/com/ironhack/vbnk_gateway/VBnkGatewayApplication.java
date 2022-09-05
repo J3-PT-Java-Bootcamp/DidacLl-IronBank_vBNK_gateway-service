26c044b0ad6dc8964abe0f3ee29e38b2548657f3
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-05 02:46:50 +0200

    BASIC_ARCH: all services created, and develop structure under vBNK_sys

---
+++ src/main/java/com/ironhack/vbnk_gateway/VBnkGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.vbnk_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VBnkGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VBnkGatewayApplication.class, args);
+    }
+
+}
