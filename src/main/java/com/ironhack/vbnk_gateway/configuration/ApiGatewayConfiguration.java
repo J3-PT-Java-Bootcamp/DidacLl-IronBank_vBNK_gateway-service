6a376258881078d0d34be2e1b65b180e37f23313
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-05 21:41:53 +0200

    Configure connexions

--- src/main/java/com/ironhack/vbnk_gateway/configuration/ApiGatewayConfiguration.java
+++ src/main/java/com/ironhack/vbnk_gateway/configuration/ApiGatewayConfiguration.java
@@ -1,5 +1,4 @@
 package com.ironhack.vbnk_gateway.configuration;
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
